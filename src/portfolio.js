e3ffd1d8cc69b8764cf7ba2865ee8ea2f601dcab
Vaibhav Nagre <vaibhavnagre@Vaibhavs-iMac.lan>
2023-09-02 17:36:59 -0700

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,20 +26,16 @@ const greeting = {
     "Talented Software Engineer equipped with coding, debugging and project management abilities. Accomplishes project goals consistently with elegant, scalable code. Works great with team members under Agile and Scrum frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/vaibhav-nagre",
+  linkedin: "https://www.linkedin.com/in/nagre/",
+  gmail: "nagre.vaibhav8@gmail.com",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
